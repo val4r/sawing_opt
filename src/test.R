0ebd9243c73071962cb5a1c340730bfd4942e907
unknown <valtteri.vironmaki@gmail.com>
2024-02-11 23:59:32 +0200

    123

--- src/test.R
+++ src/test.R
@@ -1,3 +1,19 @@
-source("functions.R")
+library(here)
+source(here("src/functions.R"))
 
 
+prices <- c(52, 120, 123, 612);
+weights <- c(42, 73, 91, 57);
+
+capacity <- 901;
+
+cut_flitch(capacity, weights, prices)
+
+r <- 250
+thick <- 50
+abs <- get_flitch_saw_points(r, thick)
+
+for (points in abs) {
+  width <- get_flitch_width(r, points[1], points[2])
+  print(width)
+}
