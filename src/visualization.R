7d5cd07ddb5c86101614973081ae51eb936dae7a
Valtteri <valtteri.vironmaki@gmail.com>
2024-02-19 22:53:34 +0200

    keskiarvoistamsta

--- src/visualization.R
+++ src/visualization.R
@@ -1,5 +1,9 @@
 library(here)
 library("assertthat")
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+
 source(here("src/functions.R"))
 
 path1 <- paste0(here(), "/data/")
@@ -12,5 +16,34 @@ production <- readRDS(paste0(path1, "prod_all_iters_1.rds"))
 
 #lasketaan keskiarvot simulaatioiteraatioiden yli
 
+#testikase
 
+max_log <- max(sapply(production, length)) #eri simulaatioiteraatioiden maksimi tukkimäärä
+
+for (i in seq(length(production))) {
+  nam <- paste("prod_",i, sep = "")
+  assign(nam, production[[i]])
+  
+  nam_df <- paste("prod_df_",i, sep = "")
+  assign(nam_df, as.data.frame(do.call(rbind, get(nam))))
+  #muutetaan jokainen df sisältämään yhtä monta riviä
+  while(nrow(get(nam_df))<max_log) {
+    assign(nam_df, rbind(get(nam_df),0))
+  }
+}
+
+
+
+cumulative_prod_1 <- as.data.frame(apply(prod_1_df, 2, cumsum)) %>% 
+  mutate(log_number = row_number()) %>% 
+  pivot_longer(V1:V5) 
+
+
+# Plot using ggplot
+ggplot(cumulative_prod_1, aes(x = log_number, y = value, color = name)) +
+  geom_line() +
+  labs(title = "Cumulative Sums of Numerical Values",
+       x = "Row Index",
+       y = "Cumulative Sum") +
+  theme_minimal()
 
