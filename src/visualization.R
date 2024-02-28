f669ce208639ff518c27724c47d6d6370dd2f65d
Valtteri <valtteri.vironmaki@gmail.com>
2024-02-28 20:32:56 +0200

    lisää visua

--- src/visualization.R
+++ src/visualization.R
@@ -5,7 +5,9 @@ library(ggplot2)
 library(tidyr)
 library(data.table)
 library(scales)
-
+library(reshape2)
+library(plotly)
+library(patchwork)
 source(here("src/functions.R"))
 
 path1 <- paste0(here(), "/data/")
@@ -62,21 +64,58 @@ ggplot(long_mean, aes(x = log_number, y = value)) +
 thick <- 48
 ws <- c(21, 48, 73, 125, 150)
 
-test <- calc_all_utils(thick, ws, production_obj_1_orderB, radius_obj_1_orderB)
-test_df <- as.data.frame(test) %>% 
+util_list <- calc_all_utils(thick, ws, production_obj_2_orderB, radius_obj_2_orderB)
+util_df <- as.data.frame(util_list) %>% 
   rename("r" = 1,
          "util_rate" = 2,
-         "idx" = 3) 
+         "idx" = 3)
 
-ggplot(test_df, aes(x=r, y=util_rate, color=idx)) + 
-  geom_point() + 
-  scale_color_gradient(low="blue", high="red") +
-  labs(title = "Sahattujen tukkien hyötyaste",
-     x = "Tukin säde",
-     y = "Tukin hyötyaste",
-     color = "Tukin järjestysluku")
+#otetaan näyte koko datasta ettei mene ihan puuroksi
+sample_util_df <- util_df[sample(nrow(util_df), 3000), ]
+  
 
-# plot( test_df$idx, test_df$util_rate)
+p_2_B <- ggplot(sample_util_df, aes(x=r, y=util_rate, color=idx)) + 
+          geom_point() + 
+          scale_color_gradient(low="red", high="green") +
+          labs(
+             x = "Tukin säde (mm)",
+             y = "Tukin hyötyaste",
+             color = "Tukin järjestysluku")
+
+(p_1_A | p_1_B) /
+(p_2_A | p_2_B) /
+(p_3_A | p_3_B) + 
+  plot_annotation(title = "Tukin hyötyaste säteen ja järjestysluvun funktiona",
+                  tag_levels = 'A')
+
+
+
+# 3D plotteja
+#
+# plot_ly(sample_util_df, z = ~util_rate, x = ~r, y = ~idx) %>%
+#   layout(scene = list(xaxis = list(title = "X-axis"),
+#                       yaxis = list(title = "Y-axis"),
+#                       zaxis = list(title = "Z-axis")))%>%
+#   add_surface(x = ~sample_util_df$r, y = ~sample_util_df$idx, z = ~sample_util_df$util_rate)
+# 
+# 
+# library(mgcv)
+# mod <- gam(util_rate ~ te(r) + te(idx) + ti(r, idx), data=sample_util_df)
+# 
+# r.seq <- seq(min(sample_util_df$r, na.rm=TRUE), max(sample_util_df$r, na.rm=TRUE), length=25)
+# idx.seq <- seq(min(sample_util_df$idx, na.rm=TRUE), max(sample_util_df$idx, na.rm=TRUE), length=25)
+# 
+# predfun <- function(x,y){
+#   newdat <- data.frame(r = x, idx=y)
+#   predict(mod, newdata=newdat)
+# }
+# 
+# fit <- outer(r.seq, idx.seq, Vectorize(predfun))
+# 
+# plot_ly() %>% 
+#   # add_markers(x = ~sample_util_df$r, y=sample_util_df$idx, z=sample_util_df$util_rate) %>% 
+#   add_surface(x = ~r.seq, y = ~idx.seq, z = t(fit))
+
 
 #Käytettyjen tukkien jakauma:
 mean(number_of_logs_obj_1_orderA)
@@ -86,4 +125,38 @@ mean(number_of_logs_obj_1_orderB)
 mean(number_of_logs_obj_2_orderB)
 mean(number_of_logs_obj_3_orderB)
 
-hist(number_of_logs_obj_2_orderA, breaks=10, main="Histogram of Data", xlab="Values", ylab="Frequency", col="lightblue", border="black")
+sd(number_of_logs_obj_1_orderA)
+sd(number_of_logs_obj_2_orderA)
+sd(number_of_logs_obj_3_orderA)
+sd(number_of_logs_obj_1_orderB)
+sd(number_of_logs_obj_2_orderB)
+sd(number_of_logs_obj_3_orderB)
+
+number_of_logs_df <- data.frame(number_of_logs_obj_1_orderA,   
+                                number_of_logs_obj_2_orderA,
+                                number_of_logs_obj_3_orderA,
+                                number_of_logs_obj_1_orderB,
+                                number_of_logs_obj_2_orderB,
+                                number_of_logs_obj_3_orderB) %>% 
+  rename("Tavoite 1, tilauskirja 1" = 1,
+         "Tavoite 2, tilauskirja 1" = 2,
+         "Tavoite 3, tilauskirja 1" = 3,
+         "Tavoite 1, tilauskirja 2" = 4,
+         "Tavoite 2, tilauskirja 2" = 5,
+         "Tavoite 3, tilauskirja 2" = 6,)
+
+number_of_logs_df_long <- reshape2::melt(number_of_logs_df)
+
+
+means <- colMeans(number_of_logs_df)
+# Plotting histograms using 'ggplot2'
+ggplot(number_of_logs_df_long, aes(x=value, fill=variable)) +
+  geom_histogram(bins=10, alpha=0.5, position='identity') +
+  facet_wrap(~variable, scales='free') +
+  labs(x='Tukkien lukumäärä', y='Lukumäärä') +
+  geom_vline(aes(xintercept=means[variable]), color='red', linetype='dashed', size=1) +
+  ggtitle('Sahattujen tukkien lukumäärän jakauma simulaatiossa') +
+  scale_x_continuous(breaks = seq(30, 70, by = 2)) +
+  theme(legend.title=element_blank())
+
+
